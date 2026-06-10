77d3f933d341e0159fdaab19b178401327d4543d
Salma.Ashour@vodafone.com <Salma.Ashour@vodafone.com>
2026-06-11 00:35:26 +0300

    Initial Commit

---
+++ UniversitiesBrowser/UniversitiesBrowserApp.swift
@@ -0,0 +1,17 @@
+//
+//  UniversitiesBrowserApp.swift
+//  UniversitiesBrowser
+//
+//  Created by Salma Ashour, Vodafone on 11/06/2026.
+//
+
+import SwiftUI
+
+@main
+struct UniversitiesBrowserApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
